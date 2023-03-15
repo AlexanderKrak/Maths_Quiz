312ae537ee97ababb4a23d68c549ec89eca6c9e6
Alexander Krakoczki <alexander.krakoczki@gmail.com>
2023-03-15 10:03:58 +0100

    Ready

--- Maths_Quiz/Form1.Designer.cs
+++ Maths_Quiz/Form1.Designer.cs
@@ -31,14 +31,18 @@ namespace Maths_Quiz
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.label_timer = new System.Windows.Forms.Label();
             this.label_left = new System.Windows.Forms.Label();
             this.label_right = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label_symbol = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button_startGame = new System.Windows.Forms.Button();
-            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label_total = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_time = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -51,20 +55,10 @@ namespace Maths_Quiz
             this.label1.TabIndex = 0;
             this.label1.Text = "Time Left";
             // 
-            // label_timer
-            // 
-            this.label_timer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label_timer.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label_timer.Location = new System.Drawing.Point(278, 16);
-            this.label_timer.Name = "label_timer";
-            this.label_timer.Size = new System.Drawing.Size(167, 44);
-            this.label_timer.TabIndex = 1;
-            this.label_timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // label_left
             // 
             this.label_left.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label_left.Location = new System.Drawing.Point(24, 90);
+            this.label_left.Location = new System.Drawing.Point(20, 1);
             this.label_left.Name = "label_left";
             this.label_left.Size = new System.Drawing.Size(50, 50);
             this.label_left.TabIndex = 2;
@@ -74,70 +68,97 @@ namespace Maths_Quiz
             // label_right
             // 
             this.label_right.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label_right.Location = new System.Drawing.Point(188, 90);
+            this.label_right.Location = new System.Drawing.Point(176, 1);
             this.label_right.Name = "label_right";
             this.label_right.Size = new System.Drawing.Size(50, 50);
             this.label_right.TabIndex = 4;
             this.label_right.Text = "?";
             this.label_right.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label5
+            // label_symbol
             // 
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(114, 90);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 50);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "?";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label_symbol.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_symbol.Location = new System.Drawing.Point(99, 1);
+            this.label_symbol.Name = "label_symbol";
+            this.label_symbol.Size = new System.Drawing.Size(50, 50);
+            this.label_symbol.TabIndex = 5;
+            this.label_symbol.Text = "?";
+            this.label_symbol.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(266, 90);
+            this.label3.Location = new System.Drawing.Point(232, 1);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 50);
             this.label3.TabIndex = 6;
             this.label3.Text = "=";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label6
+            // panel1
             // 
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(345, 90);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(100, 50);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "?";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 165);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(473, 125);
+            this.panel1.TabIndex = 11;
             // 
-            // button_startGame
+            // panel2
             // 
-            this.button_startGame.Location = new System.Drawing.Point(114, 296);
-            this.button_startGame.Name = "button_startGame";
-            this.button_startGame.Size = new System.Drawing.Size(284, 29);
-            this.button_startGame.TabIndex = 8;
-            this.button_startGame.Text = "I wanna play a game";
-            this.button_startGame.UseVisualStyleBackColor = true;
-            this.button_startGame.Click += new System.EventHandler(this.button_startGame_Click);
+            this.panel2.Controls.Add(this.label_total);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label_left);
+            this.panel2.Controls.Add(this.label_symbol);
+            this.panel2.Controls.Add(this.label_right);
+            this.panel2.Location = new System.Drawing.Point(3, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(436, 51);
+            this.panel2.TabIndex = 12;
             // 
-            // timer
+            // label_total
             // 
-            this.timer.Interval = 1000;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            this.label_total.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_total.Location = new System.Drawing.Point(309, 0);
+            this.label_total.Name = "label_total";
+            this.label_total.Size = new System.Drawing.Size(50, 50);
+            this.label_total.TabIndex = 7;
+            this.label_total.Text = "?";
+            this.label_total.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("SimSun", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(473, 37);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Press Q for +, W for - and E for x";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_time.Location = new System.Drawing.Point(314, 27);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(41, 29);
+            this.label_time.TabIndex = 12;
+            this.label_time.Text = "10";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(497, 353);
-            this.Controls.Add(this.button_startGame);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label_right);
-            this.Controls.Add(this.label_left);
-            this.Controls.Add(this.label_timer);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -146,6 +167,9 @@ namespace Maths_Quiz
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Maths Quiz";
             this.TransparencyKey = System.Drawing.Color.Black;
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,14 +178,16 @@ namespace Maths_Quiz
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label_timer;
         private System.Windows.Forms.Label label_left;
         private System.Windows.Forms.Label label_right;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_symbol;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button_startGame;
-        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_total;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_time;
     }
 }
 
--- Maths_Quiz/Form1.cs
+++ Maths_Quiz/Form1.cs
@@ -6,36 +6,117 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Maths_Quiz
 {
     public partial class Form1 : Form
     {
+        
+        private int seconds = 0;
+        
+        int numA;
+        int numB;
+        int total;
+
+        Random rnd = new Random();
+        string[] maths = { "Add", "Subtract", "Multiply" };
+
+        string secretAnswer;
+        string userChoice;
+
         public Form1()
         {
             InitializeComponent();
+            SetUpGame();
         }
-        private int counter = 10;
-        private void label2_Click(object sender, EventArgs e)
+
+        private void SetUpGame()
         {
+            seconds = 10;
+            timer1.Start();
+            numA = rnd.Next(10, 20);
+            numB = rnd.Next(0, 9);
 
+            secretAnswer = maths[rnd.Next(0, maths.Length)];
+
+            switch (secretAnswer)
+            {
+                case "Add":
+                    total = numA + numB;
+                    break;
+                case "Subtract":
+                    total = numA - numB;
+                    break;
+                case "Multiply":
+                    total = numA * numB;
+                    break;
+            }
+
+            label_left.Text = numA.ToString();
+            label_right.Text = numB.ToString();
+            label_total.Text = total.ToString();
+            label_symbol.Text = "?";
         }
 
-        private void button_startGame_Click(object sender, EventArgs e)
+        private void CheckAnswer()
         {
-            timer = new Timer();
-            timer.Start();
-            label_timer.Text = counter.ToString();
+            if (userChoice == secretAnswer)
+            {
+                timer1.Stop();
+                MessageBox.Show("Correct. Try it again");
+                SetUpGame();
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Incorrect");
+                label_symbol.Text = "?";
+            }
         }
 
+        //private void button_startGame_Click(object sender, EventArgs e)
+        //{
+        //    button_startGame.Enabled = false;
+        //    seconds = 10;
+        //    countdown_timer.Start();
+        //}
+        //
+        //
         private void timer_Tick(object sender, EventArgs e)
         {
-            counter--;
-            if (counter == 0)
-                timer.Stop();
-            label_timer.Text = counter.ToString();
-            //Console.WriteLine("Your time is up");
+            label_time.Text = seconds--.ToString();
+            if(label_time.Text == "0")
+            {
+                timer1.Stop();
+                label_time.Text = "You re sucks";
+                //button_startGame.Enabled = true;
+        
+            }
+        }
+        
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q)
+            {
+                userChoice = "Add";
+                label_symbol.Text = "+";
+                CheckAnswer();
+
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                userChoice = "Subtract";
+                label_symbol.Text = "-";
+                CheckAnswer();
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                userChoice = "Multiply";
+                label_symbol.Text = "x";
+                CheckAnswer();
+            }
         }
     }
 }
